6e143f5a22f2dea1e922864b6d28df567811f28d
SATISH KUMAR <satishkumarofficial74@gmail.com>
2022-02-09 15:26:22 +0530

    Initail Commit

--- Configuration/ConfigTest.py
+++ Configuration/ConfigTest.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -22,3 +25,15 @@ def init_driver(request):
     driver.delete_all_cookies()
     yield
     driver.quit()
+
+    """Clean up the HTML directory to generate new HTML report"""
+    folder = 'HTML_Reports'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
