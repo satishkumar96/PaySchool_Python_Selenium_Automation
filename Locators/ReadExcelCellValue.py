adcad9950e06cdcc075038b3eab8104e88c43ba3
SATISH KUMAR <satishkumarofficial74@gmail.com>
2022-02-10 13:24:53 +0530

    few changes Commit

--- Locators/ReadExcelCellValue.py
+++ Locators/ReadExcelCellValue.py
@@ -2,7 +2,7 @@ import openpyxl
 
 
 def read_data_from_excel(sheetname, rownum, colnum):
-    file = "C:\\Users\\SATISH\\PycharmProjects\\PaySchool_Python_Selenium_Automation\\ExcelSheet\\LocatorsValue.xlsx"
+    file = 'ExcelSheet/LocatorsValue.xlsx'
     workbook = openpyxl.load_workbook(file)
     sheet = workbook[sheetname]
     inputvalue = sheet.cell(row=rownum, column=colnum).value
