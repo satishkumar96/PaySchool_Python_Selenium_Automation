1799d017f82fc43d4e29dd62b934b631efe275d3
SATISH KUMAR <satishkumarofficial74@gmail.com>
2022-02-11 12:46:27 +0530

    few changes Commit

--- Configuration/ConfigTest.py
+++ Configuration/ConfigTest.py
@@ -9,7 +9,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 
 
-@pytest.fixture(params=["chrome"], scope="class")
+@pytest.fixture(params=["chrome","firefox"], scope="class")
 def init_driver(request):
     if request.param == "chrome":
         service = ChromeService(executable_path=ChromeDriverManager().install())
